609f99b488cad3de607b386ee7b04e282390e1b6
Lucas Stirpe <49053857+lucasnahuels@users.noreply.github.com>
2019-03-31 01:16:28 -0300

    Add files via upload

---
+++ CControladora.cs
@@ -0,0 +1,58 @@
+﻿using System;
+namespace Alumnos
+{
+    public class CControladora
+    {
+        public static void Main()
+        {
+            CAlumnos listadoAlumnos = new CAlumnos();
+            char opcion;
+            ulong auxLegajo;
+            do
+            {
+                opcion = Convert.ToChar(CInterfaz.darOpcion().ToUpper());
+                //.ToUpper() Convierte la cadena a MAYÚSCULAS.
+                switch (opcion)
+                {
+                    case 'C':
+                        listadoAlumnos.setCUOTA(Convert.ToSingle(CInterfaz.pedirDato("Cuota General")));
+                        break;
+                    case 'G':
+                        CInterfaz.mostrarInfo(Convert.ToString(listadoAlumnos.getCUOTA()));
+                        break;
+                    case 'A':
+                        auxLegajo = Convert.ToUInt64(CInterfaz.pedirDato("Legajo"));
+                        string auxApellidos = CInterfaz.pedirDato("Apellidos");
+                        string auxNombres = CInterfaz.pedirDato("Nombres");
+                        float auxBeca =Convert.ToSingle( CInterfaz.pedirDato("Beca"));
+                        if (!listadoAlumnos.crearAlumno(auxLegajo, auxApellidos, auxNombres, auxBeca))
+                        {
+                            CInterfaz.mostrarInfo("Legajo Preexistente");
+                        }
+                        break;
+                    case 'M':
+                        auxLegajo = Convert.ToUInt64(CInterfaz.pedirDato("Legajo"));
+                        CInterfaz.mostrarInfo(listadoAlumnos.darDatos(auxLegajo));
+                        break;
+                     case 'L':
+                        listadoAlumnos.ordenar();
+                        CInterfaz.mostrarInfo(listadoAlumnos.darDatos());
+                        break;
+                    case 'R':
+                        auxLegajo = Convert.ToUInt64(CInterfaz.pedirDato("Legajo"));
+                        if (!listadoAlumnos.eliminarAlumno(auxLegajo))
+                        {
+                            CInterfaz.mostrarInfo("Alumno Inexistente");
+                        }
+                        break;
+                    case 'S':
+                        break;
+                    default:
+                        CInterfaz.mostrarInfo("Opción inválida");
+                        break;
+                }
+            } while (opcion != 'S');
+        }
+    }
+}
+
---
+++ CCuentas.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+namespace Alumnos
+{
+    public class CAlumnos
+    {
+        //Variables Miembro
+        private ArrayList listado;
+
+        //Constructor
+        public CAlumnos()
+        {
+            this.listado = new ArrayList();
+        }
+        //Metodos
+        public CAlumno buscaAlumno(ulong leg)
+        {
+            foreach (CAlumno aux in this.listado)
+            {
+                if (aux.getLegajo() == leg) return aux;
+            }
+            return null;
+        }
+
+        public bool crearAlumno(ulong leg, string ape, string nom, float bec)
+        {
+            if (this.buscaAlumno(leg) == null)
+            {
+                this.listado.Add(new CAlumno(leg,ape,nom,bec));
+                return true;
+            }
+            return false;
+        }
+
+        public string darDatos(ulong leg)
+        {
+            CAlumno aux = this.buscaAlumno(leg);
+            if (aux != null) return aux.darDatos();
+            return "Alumno inexistente";
+        }
+
+        public string darDatos()
+        {
+
+            if (this.listado.Count != 0)
+            {
+                String datos = "";
+                foreach (CAlumno aux in this.listado) datos += aux.darDatos() + "\n";
+                return datos;
+            }
+            return "No se registraron alumnos válidos";
+        }
+
+        public bool eliminarAlumno(ulong leg)
+        {
+            CAlumno aux = this.buscaAlumno(leg);
+            if (aux != null)
+            {
+                this.listado.Remove(aux);
+                return true;
+            }
+            return false;
+        }
+
+        public void setCUOTA(float cuo)
+        {
+            CAlumno.setCUOTA(cuo);
+        }
+
+        public float getCUOTA()
+        {
+            return CAlumno.getCUOTA();
+        }
+        public float darCuotaMensual(ulong leg)
+        {
+            CAlumno aux = this.buscaAlumno(leg);
+            if (aux != null) return aux.darCuotaMensual();
+            return 0.0f;
+        }
+        public void ordenar()
+        {
+            for (int i = 0; i < this.listado.Count - 1; i++)
+            {
+                for (int j = i + 1; j < this.listado.Count; j++)
+                {
+                    if (((CAlumno)this.listado[i]).getLegajo() > ((CAlumno)this.listado[j]).getLegajo())
+                    {
+                        CAlumno auxCta = (CAlumno)this.listado[i];
+                        this.listado[i] = this.listado[j];
+                        this.listado[j] = auxCta;
+                    }
+                }
+            }
+        }
+    }
+}
+
